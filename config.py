49a0061474dcd7bc57d45d7b2c00fa712818a56c
gvaletin2016 <gvalentin@gmx.com>
2018-03-19 12:03:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ STRATUM_PORT = 5221
 # Coin address where money goes. If you mine direct to the exchange, you MUST specify payment_id together with wallet of exchange.
 WALLET = 'Se3vmM2qMQu7CEGF76RtXJ43AJeCbVP5QSeQuGoDbR3RP81VJhNbeAMFPoaGm5FfRWEybxHxQoGj2H6idPq5iq4o1Tkqma6oQ'
 # Only if you mine direct to the exchange
-PAYMENT_ID = ''
+PAYMENT_ID = 'Se3vmM2qMQu7CEGF76RtXJ43AJeCbVP5QSeQuGoDbR3RP81VJhNbeAMFPoaGm5FfRWEybxHxQoGj2H6idPq5iq4o1Tkqma6oQ'
 
 # It's useful for individually monitoring and statistic.
 # In your workers you have to use any number as username (without wallet!)
