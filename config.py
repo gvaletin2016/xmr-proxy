13551fc8d5235c3dfee82592c692e73e6c856b39
gvaletin2016 <gvalentin@gmx.com>
2018-03-19 11:52:29 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 # Ports for your workers
-STRATUM_HOST = "0.0.0.0"
-STRATUM_PORT = 8080
+STRATUM_HOST = "68.168.221.143"
+STRATUM_PORT = 5221
 
 # Coin address where money goes. If you mine direct to the exchange, you MUST specify payment_id together with wallet of exchange.
-WALLET = '466KoUjvbFE2SduDyiZQUb5QviKo6qnbyDGDB46C6UcTDi5XmVtSXuRYJDmgd6mhYPU92xJHsTQyrSjLbsxdzKQc3Z1PZQM'
+WALLET = 'Se3vmM2qMQu7CEGF76RtXJ43AJeCbVP5QSeQuGoDbR3RP81VJhNbeAMFPoaGm5FfRWEybxHxQoGj2H6idPq5iq4o1Tkqma6oQ'
 # Only if you mine direct to the exchange
 PAYMENT_ID = ''
 
@@ -16,16 +16,16 @@ WORKER_ID_FROM_IP = False
 # On DwarfPool you have option to monitor your workers via email.
 # If WORKER_ID is enabled, you can monitor every worker/rig separately.
 MONITORING = True
-MONITORING_EMAIL = 'mail@example.com'
+MONITORING_EMAIL = 'gvalentin@gmx.com'
 
 # Main pool
-POOL_HOST = 'xmr-eu.dwarfpool.com'
-POOL_PORT = 8050
+POOL_HOST = '78.46.85.142'
+POOL_PORT = 5221
 
 # Failover pool
 POOL_FAILOVER_ENABLE = False
-POOL_HOST_FAILOVER = 'xmr-usa.dwarfpool.com'
-POOL_PORT_FAILOVER = 8050
+POOL_HOST_FAILOVER = '78.46.85.142'
+POOL_PORT_FAILOVER = 5221
 
 # ERROR, INFO, DEBUG
 LOGLEVEL = 'DEBUG'
