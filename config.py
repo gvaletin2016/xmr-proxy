182b00c6af84c49f06867a32ac37e7ed956970a6
gvaletin2016 <gvalentin@gmx.com>
2018-03-25 01:16:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
 # Ports for your workers
-STRATUM_HOST = "68.168.221.143"
-STRATUM_PORT = 3333
+STRATUM_HOST = "64.20.49.26"
+STRATUM_PORT = 3334
 
 # Coin address where money goes. If you mine direct to the exchange, you MUST specify payment_id together with wallet of exchange.
-WALLET = '28nLfLUi8CQBcSgo1JqqdqPHQgSuFxSgF4aFbau6eD4dJ1CDTf6XEUM43z54VwGg1YhsTGKbiwXa4GpBZHdR98MA28xro28'
+WALLET = 'N8jMWXcNWznRPQfeaAPRFXWZRZjFduyQtLH5qQfwpzsbRrVByRGJXHcMv1J98ExAkNBnhnbnPVUKjWwYBXahYdjNPG8XCTZ'
 # Only if you mine direct to the exchange
-PAYMENT_ID = '28nLfLUi8CQBcSgo1JqqdqPHQgSuFxSgF4aFbau6eD4dJ1CDTf6XEUM43z54VwGg1YhsTGKbiwXa4GpBZHdR98MA28xro28'
+PAYMENT_ID = ''
 
 # It's useful for individually monitoring and statistic.
 # In your workers you have to use any number as username (without wallet!)
@@ -19,12 +19,12 @@ MONITORING = True
 MONITORING_EMAIL = 'gvalentin@gmx.com'
 
 # Main pool
-POOL_HOST = 'bytecoin.pt'
-POOL_PORT = 3333
+POOL_HOST = 'us-nbr.4miner.me'
+POOL_PORT = 3334
 
 # Failover pool
 POOL_FAILOVER_ENABLE = False
-POOL_HOST_FAILOVER = 'bytecoin.pt'
+POOL_HOST_FAILOVER = 'nbr.ciapool.com'
 POOL_PORT_FAILOVER = 3333
 
 # ERROR, INFO, DEBUG
